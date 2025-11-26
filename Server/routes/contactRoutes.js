e069d63dea7b1c7cab3c9e37cf425e24982cbdb7
ChaniChalamish <b0527626958@gmail.com>
2025-11-26 15:09:04 +0200

    added contact modal

--- Server/routes/contactRoutes.js
+++ Server/routes/contactRoutes.js
@@ -4,5 +4,8 @@ const { receiveContact } = require('../controllers/contactController');
 const ApiRateLimiter = require('../middlewares/apiRate');
 
 router.post('/', receiveContact);
+router.get('/', ApiRateLimiter, getContacts);
+router.put('/:id', ApiRateLimiter, updateContactStatus);
+router.delete('/:id', ApiRateLimiter, deleteContact);
 
 module.exports = router;
